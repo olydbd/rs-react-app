c1e09f9e8fe9e60af3da9c8d9b7524602f488b05
Olya Dubodel <dubodel.olya@gmail.com>
2025-07-18 22:04:29 +0300

    refactor: use form submission instead of button click in Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,5 +1,11 @@
-import { Component, type ChangeEvent, type ReactNode } from 'react';
+import {
+  Component,
+  type ChangeEvent,
+  type FormEvent,
+  type ReactNode,
+} from 'react';
 import { SEARCH_KEY } from '../../utils/constants';
+import SearchButtonIcon from '../ui/icons/SearchButton';
 
 interface Props {
   initialSearch: string;
@@ -17,14 +23,15 @@ class Search extends Component<Props, State> {
       query: props.initialSearch || '',
     };
     this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e: ChangeEvent<HTMLInputElement>): void {
     this.setState({ query: e.target.value });
   }
 
-  handleClick(): void {
+  handleSubmit(e: FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
     const searchText = this.state.query.trim();
     this.setState({ query: searchText });
     localStorage.setItem(SEARCH_KEY, searchText);
@@ -33,7 +40,7 @@ class Search extends Component<Props, State> {
 
   render(): ReactNode {
     return (
-      <div className="relative mx-auto max-w-sm">
+      <form onSubmit={this.handleSubmit} className="relative mx-auto max-w-sm">
         <input
           type="text"
           className="ease w-full rounded-full border border-slate-200 bg-transparent py-4 pr-10 pl-5 text-sm text-slate-700 shadow-sm transition duration-300 placeholder:text-slate-400 hover:border-slate-300 focus:border-slate-400 focus:shadow focus:outline-none"
@@ -44,22 +51,10 @@ class Search extends Component<Props, State> {
         <button
           className="absolute top-1/2 right-2 -translate-y-1/2 cursor-pointer rounded-full border border-transparent bg-[#b2daed] p-3 text-center text-sm text-white shadow-sm transition-all hover:bg-[#6391a6] hover:shadow"
           type="button"
-          onClick={this.handleClick}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="h-4 w-4"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <SearchButtonIcon />
         </button>
-      </div>
+      </form>
     );
   }
 }
